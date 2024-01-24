c8f31652f816771208df19427f2b52a162754f10
Julian Ng <julesnuggy@gmail.com>
2024-01-24 16:29:02 +0000

    Add server code

---
+++ src/server/controllers/movie.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const jwtSecret = 'mysecret';
+
+const getAllMovies = async (req, res) => {
+    const movies = await prisma.movie.findMany();
+
+    res.json({ data: movies });
+};
+
+const createMovie = async (req, res) => {
+    const { title, description, runtimeMins } = req.body;
+
+    try {
+        const token = null;
+        // todo verify the token
+    } catch (e) {
+        return res.status(401).json({ error: 'Invalid token provided.' })
+    }
+
+    const createdMovie = null;
+
+    res.json({ data: createdMovie });
+};
+
+module.exports = {
+    getAllMovies,
+    createMovie
+};
